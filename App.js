678b4c63cac2b4092e4476f56709a074b6d383ff
Ilysse200 <ilysseted500@gmail.com>
2025-06-11 11:38:31 +0200

    Created a blog Mobile Application

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//Import React
+import React from "react";
 
-export default function App() {
-  return (
+//Import usestate hooks
+
+import { useState } from "react";
+
+//Import react native components
+import { 
+    View,       //container component for layout
+    Button,     //button component for user functionalities
+    Text,       //Reactnative component for displaying texts
+    TextInput,  //Input fields for texts
+    FlatList,   //Efficient list view for rendering posts
+    StyleSheet, //Component for custom styling
+    TouchableOpacity,   //Wrapper for making views touchable
+ } from "react-native";
+
+export default function App () {
+
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* App heading */}
+      <View style={styles.headingContainer}>
+        <Text style={styles.heading}>
+          Blog
+        </Text>
+      </View>
     </View>
-  );
+  )
 }
 
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    //Main container style for the app
+
+    container:{
+        flex:1, //Take all remaining space
+        paddingTop:40, // Space from the top
+        paddingHorizontal:20 //Space from the left
+    },
+
+    //container for the header section
+    headingContainer:{
+        backgroundColor:'#3498db',//Blue background
+        padding:10, //Pading inside the container
+        borderRadius:10 ,//Rounded corners
+        marginBottom: 20, //Space below the heading
+    },
+
+    //Style for the main heading text
+    heading:{
+      fontSize: 24, //Large fontsize
+      fontWeight: 'bold', //Bold Text
+      textAlign: 'Center', //White Text color
+      color: 'white', //White text color
+    },
+    //container for each blog post
+    postContainer:{
+
+      
+    }
 });
+
+
+
